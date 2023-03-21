941086cc8099b9d4a877ae1c9f69485f5e1b7e0c
anandarupmukherjee <anandarupmukherjee@gmail.com>
2023-03-21 11:29:26 +0000

    sqlite3 report read
    reading contents from the simulation output file

---
+++ read_data.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 11:06:37 2023
+
+@author: anandarupmukherjee
+"""
+
+import sqlite3
+
+import pandas as pd
+import sqlalchemy 
+
+try:
+    conn = sqlite3.connect('2023-02-20_Overall_Process_AP.db')    
+except Exception as e:
+    print(e)
+
+#Now in order to read in pandas dataframe we need to know table name
+cur = conn.cursor()
+# Execute a query to get a list of all tables in the database
+cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+# Fetch all the results
+tables = cur.fetchall()
+
+# Print the report header
+print("Report of all tables in the database:\n")
+
+# Print the table names
+for table in tables:
+    table_name = table[0]
+    print("Table:", table_name)
+    cur.execute(f"SELECT * FROM {table_name}")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+# Print the report footer
+print("\nTotal tables: ", len(tables))
+
+# Close the cursor and the database connection
+cur.close()
+conn.close()
